f20ddcb73a29293e1c28fba07fea2a1394352ff3
Nick <nick.pestov@mail.utoronto.ca>
2026-07-01 14:05:04 -0400

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_len = len(s1)
+        s2_len = len(s2)
+        s1 = sorted(s1)
+        i = 0
+        while i + s1_len - 1 < s2_len:
+            sub = s2[i: i + s1_len]
+            sub = sorted(sub)
+            if sub == s1:
+                return True
+            i += 1
+        return False
